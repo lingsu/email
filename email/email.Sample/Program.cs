8fb18d3fd859966b2e868c75f43a18a07cb2efc5
xins Bin <q25a25q@live.com>
2015-08-29 09:40:43 +0800

    add a sample

---
+++ email/email.Sample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using email.Delivery;
+using email.Providers;
+using email.Templates;
+
+namespace email.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IEmailProvider ep = new SmtpEmailProvider("smtp.qq.com", 25, new NetworkCredential("570678569", "q284655"));
+            IEmailTemplateEngine tn = new DotLiquidEmailTemplateEngine();
+            IDeliveryConfiguration dc = new DeliveryConfiguration();
+
+            DeliveryService service = new DeliveryService(ep, dc);
+
+            var tos = new List<string>()
+            {
+                "xiaxw@guoji.biz",
+                "xuedz@guoji.biz",
+                "shgzs@guoji.biz"
+            };
+            var message = tn.CreateTextEmail("用户{{ UserName }}兑换了 aa，请及时处理!", new { UserName = "自定义数据", From = "570678569@qq.com", To = "q25a25q@live.com", Subject = "邮件主题" });
+            message.Cc = tos;
+            ep.Send(message);
+
+            service.Start();
+
+            service.Send(message);
+
+            Console.WriteLine("success ");
+            Console.Read();
+            service.Stop();
+        }
+    }
+}
